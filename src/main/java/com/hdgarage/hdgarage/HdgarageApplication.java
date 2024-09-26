fae325494e47aaabe416940ea0eb151e2d9c684e
Holylogus <hamar.patrik94@gmail.com>
2024-09-26 09:56:33 +0200

    create project

---
+++ src/main/java/com/hdgarage/hdgarage/HdgarageApplication.java
@@ -0,0 +1,13 @@
+package com.hdgarage.hdgarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdgarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdgarageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hdgarage/hdgarage/HdgarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdgarage.hdgarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdgarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
